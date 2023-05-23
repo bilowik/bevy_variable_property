fc4e195bd15e2de2f574a55c663ddf8ee00511be
bilowik <aaronjflores962@gmail.com>
2023-05-22 22:38:53 -0400

    Add the first unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use array_macro::array;
 use bevy::prelude::*;
 
 use rand::{
@@ -83,6 +82,16 @@ where
     }
 }
 
+impl<T, const N: usize> From<[T; N]> for Property<T>
+where
+    T: Clone + PartialOrd + SampleUniform + Send + Sync + Default + 'static + Reflect + FromReflect,
+    Standard: Distribution<T>,
+{
+    fn from(value: [T; N]) -> Self {
+        Property::RandomChoice(value.into())
+    }
+}
+
 /// Provides `Static(T::default())`
 impl<T> Default for Property<T>
 where
@@ -94,109 +103,41 @@ where
     }
 }
 
-// The reason Vec2Property and Vec3Property don't utilize VecProperty internally is mainly for
-// efficiency and ease of access and mutation.
-
-/// A Vec2 where x and y are `Property<f32>`, see [Property] for more information.
-#[derive(Reflect, FromReflect, Clone, Default)]
-pub struct Vec2Property {
-    pub x: Property<f32>,
-    pub y: Property<f32>,
-}
-
-impl Vec2Property {
-    pub fn new(x: Property<f32>, y: Property<f32>) -> Self {
-        Self { x, y }
-    }
-
-    /// Generates a Vec2 from the set properties
-    pub fn get(&self) -> Vec2 {
-        Vec2::new(self.x.get_value(), self.y.get_value())
-    }
-}
-
-/// A Vec3 where x and y are `Property<f32>`, see [Property] for more information.
-#[derive(Reflect, FromReflect, Clone, Default)]
-pub struct Vec3Property {
-    pub x: Property<f32>,
-    pub y: Property<f32>,
-    pub z: Property<f32>,
-}
-
-impl Vec3Property {
-    pub fn new(x: Property<f32>, y: Property<f32>, z: Property<f32>) -> Self {
-        Self { x, y, z }
-    }
-
-    /// Generates a Vec3 from the set properties
-    pub fn get(&self) -> Vec3 {
-        Vec3::new(self.x.get_value(), self.y.get_value(), self.z.get_value())
-    }
-}
-
-/// A generic Vec-generating struct if Vec2Property and Vec3Property do not fit your needs.
-/// See [Property] for more information.
-///
-/// For example, if you a field that generates a UVec2, you could utilize this like so:
-/// ```no_run
-/// struct MyStruct {
-///     _vec: VecProperty<u32, 2>,
-/// }
-///
-/// // Getting the UVec2 is simple since all of the Vec types utilized in Bevy support
-/// // From arrays.
-///
-/// impl MyStruct {
-///     pub fn vec() -> UVec2 {
-///         self._vec.get().into()
-///     }
-/// }
-/// ```
-#[derive(Reflect, FromReflect, Clone)]
-pub struct VecProperty<T, const N: usize>
-where
-    T: Clone + PartialOrd + SampleUniform + Send + Sync + Default + 'static + Reflect + FromReflect,
-    Standard: Distribution<T>,
-{
-    pub properties: [Property<T>; N],
-}
-
-impl<const N: usize, T> VecProperty<T, N>
-where
-    T: Clone + PartialOrd + SampleUniform + Send + Sync + Default + 'static + Reflect + FromReflect,
-    Standard: Distribution<T>,
-{
-    pub fn new(properties: [Property<T>; N]) -> Self {
-        Self { properties }
-    }
-
-    /// Creates an array of T with size N from the given properties.
-    /// Can convert to one of bevy's Vec types if applicable with `into()`
-    pub fn get(&self) -> [T; N] {
-        array![i => self.properties[i].get_value(); N]
-    }
-}
-
-/// A Property-like color field that can generate random colors, select one from a set list, or
-/// provide a static one.
-#[derive(Reflect, FromReflect)]
-pub enum ColorGenerator {
-    Static(Color),
-    Range(VecProperty<f32, 4>),
-    RandomFromList(Vec<Color>),
-    Random,
-}
-
-impl ColorGenerator {
-    pub fn generate(&self) -> Color {
-        match self {
-            ColorGenerator::Static(color) => *color,
-            ColorGenerator::Range(v) => v.get().into(),
-            ColorGenerator::RandomFromList(colors) => *colors.choose(&mut thread_rng()).unwrap(),
-            ColorGenerator::Random => {
-                let mut rng = thread_rng();
-                Color::rgb_u8(rng.gen(), rng.gen(), rng.gen())
-            }
+#[cfg(test)]
+mod tests {
+    use super::{Property, VariableProperty};
+    use bevy::prelude::*;
+    #[test]
+    fn range_generation() {
+        let ranges = (2.5..5.0, -10.0..0.0, 0.0..1.0);
+        let vec3_generator: (Property<f32>, Property<f32>, Property<f32>) = (
+            ranges.0.clone().into(),
+            ranges.1.clone().into(),
+            ranges.2.clone().into(),
+        );
+        for _ in 0..10 {
+            let v: Vec3 = vec3_generator.get_value().into();
+            assert!(
+                ranges.0.contains(&v.x),
+                "{} was not in the range of ({}..{})",
+                v.x,
+                ranges.0.start,
+                ranges.0.end
+            );
+            assert!(
+                ranges.1.contains(&v.y),
+                "{} was not in the range of ({}..{})",
+                v.y,
+                ranges.1.start,
+                ranges.1.end
+            );
+            assert!(
+                ranges.2.contains(&v.z),
+                "{} was not in the range of ({}..{})",
+                v.z,
+                ranges.2.start,
+                ranges.2.end
+            );
         }
     }
 }
