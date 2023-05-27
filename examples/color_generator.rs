09f117625c7c819afdfe9e9f60df3a16bac57053
bilowik <aaronjflores962@gmail.com>
2023-05-26 23:05:49 -0400

    Improve color generator example

--- examples/color_generator.rs
+++ examples/color_generator.rs
@@ -5,43 +5,40 @@ use bevy_variable_property::prelude::*;
 
 fn main() {
     // We want the range inclusive bc in this made up case we want alpha to always be 1.0.
-    let mut p = Property::from_array_range([0.0f32, 0.5, 0.0, 1.0], [0.5, 1.0, 0.2, 1.0], true);
-    let mut color: Color;
-    println!("Generating colors in the (inclusive) range: R: 0.0-0.5, G: 0.5-1.0, B: 0.0-0.2, A: 1.0-1.0");
-    println!("===============================");
-    for _ in 0..5 {
-        color = p.get_value().into();
-        println!("{:?}", Color::from(p.get_value()));
+    let properties = [
+        (
+            Property::from_array_range([0.0f32, 0.5, 0.0, 1.0], [0.5, 1.0, 0.2, 1.0], true),
+            "Generating colors in the (inclusive) range: R: 0.0-0.5, G: 0.5-1.0, B: 0.0-0.2, A: 1.0-1.0"
+        ),
+        (
+            Property::Random,
+            "Generating entirely random colors",
+        ),
+        (
+            Property::RandomChoice(
+                vec![
+                    [0.0, 0.5, 1.0, 1.0],
+                    [0.3, 0.9, 0.5, 1.0],
+                    [0.5, 0.3, 0.3, 1.0],
+                    [0.8, 0.2, 0.9, 1.0],
+                    [1.0, 1.0, 0.9, 0.8],
+                ]
+                .into_iter()
+                .map(|e| e.into())
+                .collect()
+            ),
+            "Generating colors based on a predefined list"
+        )
+    ];
+
+    for (p, description) in properties.into_iter() { 
+        println!("{}", description);
+        println!("===============================");
+        for _ in 0..5 {
+            let color: Color = p.get_value().into();
+            println!("{:?}", color);
+        }
+        println!("===============================\n");
     }
 
-    println!("===============================\n");
-
-    p = Property::Random;
-    println!("Printing entirely random colors:");
-    println!("===============================");
-    for _ in 0..5 {
-        println!("{:?}", Color::from(p.get_value()));
-    }
-
-    println!("===============================\n");
-
-    p = Property::RandomChoice(
-        vec![
-            [0.0, 0.5, 1.0, 1.0],
-            [0.3, 0.9, 0.5, 1.0],
-            [0.5, 0.3, 0.3, 1.0],
-            [0.8, 0.2, 0.9, 1.0],
-            [1.0, 1.0, 0.9, 0.8],
-        ]
-        .into_iter()
-        .map(|e| e.into())
-        .collect(),
-    );
-
-    println!("Printing randomly from a preset list of colors:");
-    println!("===============================");
-    for _ in 0..5 {
-        println!("{:?}", Color::from(p.get_value()));
-    }
-    println!("===============================");
 }
