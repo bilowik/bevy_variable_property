917cfc2c61d2a956c1d60e0dd13c92a4c29cc02b
bilowik <aaronjflores962@gmail.com>
2023-05-28 13:27:22 -0400

    Swap description and prop

--- examples/color_generator.rs
+++ examples/color_generator.rs
@@ -7,14 +7,15 @@ fn main() {
     // We want the range inclusive bc in this made up case we want alpha to always be 1.0.
     let properties = [
         (
+            "Generating colors in the (inclusive) range: R: 0.0-0.5, G: 0.5-1.0, B: 0.0-0.2, A: 1.0-1.0",
             Property::from_array_range([0.0f32, 0.5, 0.0, 1.0], [0.5, 1.0, 0.2, 1.0], true),
-            "Generating colors in the (inclusive) range: R: 0.0-0.5, G: 0.5-1.0, B: 0.0-0.2, A: 1.0-1.0"
         ),
         (
-            Property::Random,
             "Generating entirely random colors",
+            Property::Random,
         ),
         (
+            "Generating colors based on a predefined list",
             Property::RandomChoice(
                 vec![
                     [0.0, 0.5, 1.0, 1.0],
@@ -27,11 +28,10 @@ fn main() {
                 .map(|e| e.into())
                 .collect()
             ),
-            "Generating colors based on a predefined list"
         )
     ];
 
-    for (p, description) in properties.into_iter() { 
+    for (description, p) in properties.into_iter() { 
         println!("{}", description);
         println!("===============================");
         for _ in 0..5 {
