b1ae3553948d986d70c40536d41d29538d84028c
= <=>
2023-05-31 19:04:50 -0400

    Add interval_property example

---
+++ examples/interval_property.rs
@@ -0,0 +1,53 @@
+use bevy::{
+    prelude::*,
+    sprite::MaterialMesh2dBundle,
+};
+use bevy_variable_property::{
+    prelude::*,
+    interval_property::*,
+};
+
+#[derive(Component)]
+pub struct MyIntervalProperty(pub IntervalProperty<Property<Vec2>>);
+
+impl AsMut<IntervalProperty<Property<Vec2>>> for MyIntervalProperty {
+    fn as_mut(&mut self) -> &mut IntervalProperty<Property<Vec2>> {
+        &mut self.0 
+    }
+}
+
+impl IntervalPropertyComponent for MyIntervalProperty {
+    type Property = Property<Vec2>;
+    type TargetComponent = Transform;
+
+    fn update(new_value: Vec2, target: &mut Transform) {
+        target.translation = new_value.extend(target.translation.z);
+    }
+}
+
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(update_interval_property_system_gen::<MyIntervalProperty>)
+        .run();
+}
+
+
+fn setup(
+    mut commands: Commands, 
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(Mesh::from(shape::Quad::new(Vec2::new(128.0, 128.0)))).into(),
+                material: materials.add(ColorMaterial::from(Color::WHITE)),
+                ..default()
+            },
+            MyIntervalProperty(IntervalProperty::new((Vec2::new(-250.0, -250.0)..Vec2::new(250.0, 250.0)).into(), 0.5)),
+        ));
+}
