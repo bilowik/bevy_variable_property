44eaca1fb9ce06527a9f1dd70151e45d30b721d9
= <=>
2023-05-31 20:12:48 -0400

    Improve impl macro for vecs

--- src/prop_rand.rs
+++ src/prop_rand.rs
@@ -2,7 +2,11 @@ use bevy::math::*;
 
 use array_macro::array;
 
-use rand::{Rng, RngCore};
+use rand::{
+    Rng, 
+    RngCore,
+    distributions::{Standard, Distribution},
+};
 
 use crate::prop_range::PropRange;
 
@@ -43,16 +47,14 @@ prop_rand_impl_many!(usize, isize, u8, u16, u32, u64, u128, i8, i16, i32, i64, i
 
 impl<T, const N: usize> PropRand for [T; N]
 where
-    T: PropRand + Clone,
+    T: PropRand + Clone
 {
     fn gen<R: RngCore + ?Sized>(rng: &mut R) -> Self {
-        // TODO: Is it worth the Standard: Distribution trait bounds to be able to just call
-        // rng.gen()? Will the difference in performance be worth it for N < 5?
-        // We would lose the ability to do Property<[Vec2; 4]> for example, but maybe that is okay?
-        array![_ => T::gen(rng); N]
+        array![_ => T::gen(rng); N].into()
     }
 
     fn gen_range<R: RngCore + ?Sized>(rng: &mut R, range: PropRange<[T; N]>) -> Self {
+        //Seems like it could be sluggish sampling each time, there might be a better way to implement this.
         if range.inclusive {
             array![i => T::gen_range(rng, (range.start[i].clone()..=range.end[i].clone()).into()); N].into()
         } else {
@@ -122,19 +124,19 @@ prop_rand_tuple_impls!(15 P, 14 O, 13 N, 12 M, 11 L, 10 K, 9 J, 8 I, 7 H, 6 G, 5
 // The reason we don't just use a version of the top impl of the macro is for efficiency.
 // There's 3 conversiosn being done per call.
 //
-// NOTE: If above we requiring Standard: Distribution for [T; N], then another performance
+// NOTE: If above we required Standard: Distribution for [T; N], then another performance
 // impliciation to think about is using <[T; N]>::gen(rng).into() so we would only sample
 // the rng once (I think?) and that could make the first impl below the most performant
 // potentially.
 macro_rules! prop_rand_vec_impl {
-    ($vec_type:tt, $inner_type:tt, $x:ident, $y:ident, $z:ident, $w:ident,) => {
+    ($vec_type:tt, $inner_type:tt, $size:literal) => {
         impl PropRand for $vec_type {
             fn gen<R: RngCore + ?Sized>(rng: &mut R) -> Self {
-                $vec_type::new(rng.gen(), rng.gen(), rng.gen(), rng.gen())
+                <[$inner_type; $size]>::gen(rng).into()
             }
 
             fn gen_range<R: RngCore + ?Sized>(rng: &mut R, range: PropRange<Self>) -> Self {
-                <[$inner_type; 4]>::gen_range(rng, PropRange {
+                <[$inner_type; $size]>::gen_range(rng, PropRange {
                     start: range.start.into(),
                     end: range.end.into(),
                     inclusive: range.inclusive
@@ -142,38 +144,23 @@ macro_rules! prop_rand_vec_impl {
             }
         }
     };
-    ($vec_type:tt, $inner_type:tt, $($dim_ident:ident,)+) => {
-        impl PropRand for $vec_type {
-            fn gen<R: RngCore + ?Sized>(rng: &mut R) -> Self {
-                $vec_type {
-                    $($dim_ident: rng.gen(),)+
-                }
-            }
-
-            fn gen_range<R: RngCore + ?Sized>(rng: &mut R, range: PropRange<Self>) -> Self {
-                $vec_type {
-                    $($dim_ident: $inner_type::gen_range(rng, PropRange { start: range.start.$dim_ident, end: range.end.$dim_ident, inclusive: range.inclusive }),)+
-                }
-            }
-        }
-    }
 }
 
-prop_rand_vec_impl!(Vec2, f32, x, y,);
-prop_rand_vec_impl!(Vec3, f32, x, y, z,);
-prop_rand_vec_impl!(Vec4, f32, x, y, z, w,);
+prop_rand_vec_impl!(Vec2, f32, 2);
+prop_rand_vec_impl!(Vec3, f32, 3);
+prop_rand_vec_impl!(Vec4, f32, 4);
 
-prop_rand_vec_impl!(DVec2, f64, x, y,);
-prop_rand_vec_impl!(DVec3, f64, x, y, z,);
-prop_rand_vec_impl!(DVec4, f64, x, y, z, w,);
+prop_rand_vec_impl!(DVec2, f64, 2);
+prop_rand_vec_impl!(DVec3, f64, 3);
+prop_rand_vec_impl!(DVec4, f64, 4);
 
-prop_rand_vec_impl!(UVec2, u32, x, y,);
-prop_rand_vec_impl!(UVec3, u32, x, y, z,);
-prop_rand_vec_impl!(UVec4, u32, x, y, z, w,);
+prop_rand_vec_impl!(UVec2, u32, 2);
+prop_rand_vec_impl!(UVec3, u32, 3);
+prop_rand_vec_impl!(UVec4, u32, 4);
 
-prop_rand_vec_impl!(IVec2, i32, x, y,);
-prop_rand_vec_impl!(IVec3, i32, x, y, z,);
-prop_rand_vec_impl!(IVec4, i32, x, y, z, w,);
+prop_rand_vec_impl!(IVec2, i32, 2);
+prop_rand_vec_impl!(IVec3, i32, 3);
+prop_rand_vec_impl!(IVec4, i32, 4);
 
 impl PropRand for Rect {
     fn gen<R: RngCore + ?Sized>(rng: &mut R) -> Self {
